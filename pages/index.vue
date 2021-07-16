6b5e9777bad2e3ace7551eec47dbf3aa68d38daf
Francis WIbisono <fws8787@gmail.com>
2021-07-17 00:57:09 +0700

    dependency logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,67 @@
 <template>
-  <v-container>
-    <h4>Semester 1</h4>
-    <v-row dense>
-      <v-col>
-        <v-card class='card pa-1' height = '100%' max-height='400px' min-width='30%' @click='matkul.mpkta = !matkul.mpkta' :color="matkul.mpkta==true ? '#006400' : '#1e1e1e'">
-          <v-card-title>MPKT-A</v-card-title>
-          <v-card-subtitle>Mata Kuliah Pengembangan Kepribadian Terintegrasi - A</v-card-subtitle>
-          <v-card-text>PPKn but more retarded</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card class='card pa-1' height = '100%' max-height='400px' min-width='30%' @click='matkul.mpktb = !matkul.mpktb' :color="matkul.mpktb==true ? '#006400' : '#1e1e1e'">
-          <v-card-title>MPKT-B</v-card-title>
-          <v-card-subtitle>Mata Kuliah Pengembangan Kepribadian Terintegrasi - B</v-card-subtitle>
-          <v-card-text>PPKn but more retarded, with t e c h n o l o g y</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card class='card pa-1' height = '100%' max-height='400px' min-width='30%' @click='matkul.agama = !matkul.agama' :color="matkul.agama==true ? '#006400' : '#1e1e1e'">
-          <v-card-title>MPK Agama</v-card-title>
-          <v-card-subtitle>Mata Kuliah Agama *</v-card-subtitle>
-          <v-card-text>... agama ...</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card class='card pa-1' height = '100%' max-height='400px' min-width='30%' @click='matkul.inggris = !matkul.inggris' :color="matkul.inggris==true ? '#006400' : '#1e1e1e'">
-          <v-card-title>MPK B.Ing</v-card-title>
-          <v-card-subtitle>Mata Kuliah Bahasa Inggris</v-card-subtitle>
-          <v-card-text>Free 3 sks lol</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card class='card pa-1' height = '100%' max-height='400px' min-width='30%' @click='matkul.olahragaseni = !matkul.olahragaseni' :color="matkul.olahragaseni==true ? '#006400' : '#1e1e1e'">
-          <v-card-title>MPKO/S</v-card-title>
-          <v-card-subtitle>Mata Kuliah Olahraga/Seni</v-card-subtitle>
-          <v-card-text>recreation time</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-container v-for='semester in data1' :key='semester'>
+      <h4>{{ semester.name }}</h4>
+      <v-row dense>
+        <v-col
+          v-for='matkul in semester.matkuls'
+          :key='matkul'
+          max-width='50%'
+        >
+          <v-card
+            @click='matkul.selected = !matkul.selected'
+            :color="matkul.selected==true ? '#006400' : dependencyDriver(matkul.dependency)===true ? '#F96b00': '#1e1e1e'"
+            height='100%'
+            min-height='300px'
+          >
+            <v-card-title>{{matkul.stitle}}</v-card-title>
+            <v-card-subtitle>{{matkul.ltitle}}</v-card-subtitle>
+            <v-card-text>{{matkul.description}}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'index',
-  data() {
-  return {
-      matkul :{
-        'mpkta': false,
-        'mpktb': false,
-        'agama': false,
-        'inggris': false,
-        'olahragaseni': false,
+  data: () => ({
+    data1: [],
+  }),
+  async mounted() {
+    this.data1 = JSON.parse(JSON.stringify(require('~/assets/matkul.json')))
+    await console.log(this.dependencyDriver(['MatDas 1', 'MatDas 2']))
+  },
+  methods: {
+    dependencyDriver(dependencyList){
+      let result = false
+      for(let i = 0; i<dependencyList.length; i++){
+        result = this.checkDependency(dependencyList[i])
+        if(result===false){return false}
+        else continue
       }
-    };
+      return result
+    },
+    checkDependency(value) {
+      const list = this.data1
+      console.log(list)
+      for(let SemesterCount = 0; SemesterCount<list.length; SemesterCount++){
+        console.log(list[SemesterCount])
+        const thisSemester = list[SemesterCount]
+        const thisSemesterMatkuls = thisSemester.matkuls
+        console.log(thisSemesterMatkuls)
+        for (let MatkulCount = 0; MatkulCount<thisSemesterMatkuls.length; MatkulCount++){
+          if(thisSemesterMatkuls[MatkulCount].stitle === value){
+            if(thisSemesterMatkuls[MatkulCount].selected){
+              return true
+            }
+            else return false
+          }
+        }
+      }
+    }
   }
 }
 </script>
