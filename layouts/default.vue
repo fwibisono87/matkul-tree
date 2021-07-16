f35a6ca98940d057294aab9d7f66f7dbbf3455e4
Francis WIbisono <fws8787@gmail.com>
2021-07-17 02:42:11 +0700

    add snackbar and beta marker

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <v-app class="px-2">
     <v-main>
+      <v-overlay
+        :opacity="1"
+        :value="loader"
+        width='100%'
+        color='#1e1e1e'
+      >
+        <v-row>
+          <v-progress-circular indeterminate class='mx-auto' color='#F96b00'>
+          </v-progress-circular>
+        </v-row>
+        <v-row>
+          <span class='mx-auto'>Loading!</span>
+        </v-row>
+      </v-overlay>
       <v-app-bar dense>
-        Pacil Matkul Tree
+        Pacil Matkul Tree <span style='color: orange; font-weight: bold'> -BETA- </span> Ilmu Komputer Wajib
       </v-app-bar>
       <v-container min-height='100%'>
         <nuxt />
@@ -17,6 +31,23 @@
       >
         <v-icon>mdi-arrow-up</v-icon>
       </v-btn>
+      <v-snackbar
+        :value='true'
+        :multi-line="multiLine"
+        :timeout='10000'
+      >
+        Halo! Situs ini masih dalam perkembangan!<br /> Saat ini, baru MK Wajib Ilkom Terimplementasi!<br />Mohon berikan feedback ke francis.wibisono@ui.ac.id
+        <template v-slot:action='{ attrs }'>
+          <v-btn
+            color="orange"
+            text
+            v-bind="attrs"
+            @click="snackbar = false"
+          >
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
     </v-main>
   </v-app>
 </template>
@@ -24,6 +55,16 @@
 <script>
 export default {
   name: 'default',
+  data: () => ({
+    loader: true
+  }),
+  created() {
+    console.log("created")
+  },
+  mounted(){
+    console.log("mounted")
+    this.loader = false
+  },
   methods: {
     onScroll (e) {
       if (typeof window === 'undefined') return
